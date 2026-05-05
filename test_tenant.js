f49bfaadc4bc367a61761a6fb8be2f44a93b2d2f
emre <emregunesart@icloud.com>
2026-05-06 01:52:16 +0300

    chore: cleanup debug endpoints and temp files

--- test_tenant.js
+++
@@ -1,7 +0,0 @@
-const { getTenantByDomain } = require('./src/lib/tenant.js');
-
-async function test() {
-  const tenant = await getTenantByDomain("www.pinowed.com");
-  console.log("Result:", tenant);
-}
-test();
